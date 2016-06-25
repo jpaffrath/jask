710e91eac3d9fce35df70fc058b9c51166abd56b
Julius Paffrath <juliuspaffrath@web.de>
2016-06-25 20:13:59 +0200

    Implement strings with whitespaces

--- src/jask/Tokenizer.java
+++ src/jask/Tokenizer.java
@@ -4,13 +4,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Tokenizer {
-
 	List<Token> parse(List<String> lines) {
 		List<Token> tokens = new ArrayList<Token>();
+		boolean isStringParsing = false;
+		StringBuilder builder = null;
 
 		for (String line : lines) {
 			for (String t : line.split(" ")) {
-				tokens.add(new Token(t.replace("\t", "")));
+				if (t.equals("")) continue;
+
+				char tChar[] = t.toCharArray();
+				if (tChar[0] == '"') {
+					isStringParsing = true;
+					builder = new StringBuilder();
+				}
+				else if (tChar[tChar.length-1] == '"') {
+					isStringParsing = false;
+					builder.append(" " + t);
+					tokens.add(new Token(builder.toString()));
+					continue;
+				}
+
+				if (isStringParsing) {
+					if (builder.length() > 0) {
+						builder.append(" " + t);
+					}
+					else {
+						builder.append(t);
+					}
+				}
+				else {
+					tokens.add(new Token(t.replace("\t", "")));
+				}
 			}
 		}
 
