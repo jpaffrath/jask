5f75ce3b9b54a5c136f79f6d17bf239ffff06d40
Julius Paffrath <juliuspaffrath@web.de>
2016-07-02 17:39:13 +0200

    Fix bug in Variable.java

--- src/jask/Variable.java
+++ src/jask/Variable.java
@@ -8,7 +8,7 @@ public class Variable {
 	private VariableType type;
 
 	public static boolean isNumber(String value) {
-		if (Pattern.matches("^\\-?[1-9]\\d{0,2}(\\.\\d*)?$", value)) return true;
+		if (value.contentEquals("0") || Pattern.matches("^\\-?[1-9]\\d{0,2}(\\.\\d*)?$", value)) return true;
 		return false;
 	}
 
