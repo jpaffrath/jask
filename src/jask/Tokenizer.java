eb9132a9774d24675e4ca49fa9a7e855bc43080a
Julius Paffrath <juliuspaffrath@web.de>
2016-09-13 17:22:38 +0200

    Fix bug with whitespaces in strings
    Still not working for functions!

--- src/jask/Tokenizer.java
+++ src/jask/Tokenizer.java
@@ -6,35 +6,32 @@ import java.util.List;
 public class Tokenizer {
 	List<String> parse(List<String> lines) {
 		List<String> tokens = new ArrayList<String>();
-		String strBuffer = "";
-		int lastIndex = 0;
+		String strBuffer = null;
 		boolean strParsing = false;
 
 		for (String line : lines) {
 			if (line.equals("")) continue;
 
-			if (line.contains("\"")) {
-				lastIndex = line.lastIndexOf('"');
-				strBuffer = line.substring(line.indexOf('"'), lastIndex+1);
-			}
-
 			for (String t : line.split(" ")) {
-				if (t.equals("")) continue;
-				if (t.charAt(0) == '"' && !strParsing) {
-					if (t.charAt(t.length()-1) == '"' && t.length() > 1) {
-						tokens.add(t);
+				if (t.contains("\"") && t.indexOf('"') == t.lastIndexOf('"')) {
+					if (!strParsing) {
+						strParsing = true;
+						strBuffer = new String(t + " ");
 						continue;
 					}
-					strParsing = true;
-					tokens.add(strBuffer);
-					continue;
-				}
-				if (t.charAt(t.length()-1) == '"') {
-					strParsing = false;
-					continue;
+					else {
+						strParsing = false;
+						strBuffer += t;
+						tokens.add(strBuffer);
+						strBuffer = null;
+						continue;
+					}
 				}
 
-				if (strParsing) continue;
+				if (strParsing) {
+					strBuffer += (t + " ");
+					continue;
+				}
 
 				tokens.add(t.replace("\t", ""));
 			}
