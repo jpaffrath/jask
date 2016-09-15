1404d79c2e869c2c5407d6016e6b860d51b92b55
Julius Paffrath <juliuspaffrath@web.de>
2016-09-15 21:19:49 +0200

    Implement comments

--- src/jask/Tokenizer.java
+++ src/jask/Tokenizer.java
@@ -3,16 +3,45 @@ package jask;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Tokenizer class
+ *
+ * @author Julius Paffrath
+ *
+ */
 public class Tokenizer {
+
+	/**
+	 * Splits the given input lines into tokens
+	 *
+	 * @param lines content of file as lines
+	 * @return parsed list of tokens
+	 */
 	List<String> parse(List<String> lines) {
 		List<String> tokens = new ArrayList<String>();
 		String strBuffer = null;
 		boolean strParsing = false;
+		boolean commentParsing = false;
 
 		for (String line : lines) {
 			if (line.equals("")) continue;
+			if (line.startsWith("//")) continue;
 
 			for (String t : line.split(" ")) {
+				// toggle comment parsing
+				if (t.contentEquals("*/") && commentParsing) {
+					commentParsing = false;
+					continue;
+				}
+
+				if (commentParsing) continue;
+
+				if (t.contentEquals("/*") && !commentParsing) {
+					commentParsing = true;
+					continue;
+				}
+
+				// toggle string parsing
 				if (t.contains("\"") && t.indexOf('"') == t.lastIndexOf('"')) {
 					if (!strParsing) {
 						strParsing = true;
