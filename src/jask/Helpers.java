e77441776b77599afbd5bc1db10b08b83e894eed
Gollinator <gplebanek@web.de>
2016-10-20 13:46:23 +0200

    improved program code from function readFile

--- src/jask/Helpers.java
+++ src/jask/Helpers.java
@@ -19,30 +19,18 @@ public class Helpers {
 	/**
 	 * Reads the content of a given file
 	 *
-	 * @param name name of the file
+	 * @param name
+	 *            name of the file
 	 * @return list of strings containing the files content
 	 */
 	public static List<String> readFile(String name) {
-		FileReader fileReader = null;
-		try {
-			fileReader = new FileReader(name);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		BufferedReader bufferedReader = new BufferedReader(fileReader);
 		List<String> lines = new ArrayList<String>();
 		String line = null;
-		try {
-			while ((line = bufferedReader.readLine()) != null) {
+		try (BufferedReader br = new BufferedReader(new FileReader(name))) {
+			while ((line = br.readLine()) != null) {
 				lines.add(line);
 			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			bufferedReader.close();
+			br.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -54,7 +42,8 @@ public class Helpers {
 	/**
 	 * Checks if a given path points to a file
 	 *
-	 * @param name path to file
+	 * @param name
+	 *            path to file
 	 * @return true if file exists, otherwise false
 	 */
 	public static boolean checkFile(String name) {
@@ -65,7 +54,8 @@ public class Helpers {
 	/**
 	 * Splits a given parameter string into a list of parameters
 	 *
-	 * @param param parameter string
+	 * @param param
+	 *            parameter string
 	 * @return list of strings containing parameters
 	 */
 	public static List<String> splitParams(String param) {
@@ -74,8 +64,10 @@ public class Helpers {
 		boolean stringParsing = false;
 
 		for (char c : param.toCharArray()) {
-			if (c == '"' && !stringParsing) stringParsing = true;
-			else if (c == '"' && stringParsing) stringParsing = false;
+			if (c == '"' && !stringParsing)
+				stringParsing = true;
+			else if (c == '"' && stringParsing)
+				stringParsing = false;
 
 			if (c == ':' && !stringParsing) {
 				params.add(tempString);
