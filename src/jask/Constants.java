c130037c88c6e211bb22891104f4e1884b4fc492
Julius Paffrath <juliuspaffrath@web.de>
2017-11-29 21:37:16 +0100

    Add Constant class

---
+++ src/jask/Constants.java
@@ -0,0 +1,13 @@
+package jask;
+
+/**
+ * Provides project overarching constants
+ *
+ * @author Julius Paffrath
+ *
+ */
+public final class Constants {
+	public static final String NULL = "NULL";
+	public static final String TRUE = "TRUE";
+	public static final String FALSE = "FALSE";
+}
