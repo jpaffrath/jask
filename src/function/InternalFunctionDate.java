f966629382a4c9eae5648f7e71a271c2a4eb477b
Julius Paffrath <juliuspaffrath@web.de>
2019-02-01 18:01:09 +0100

    Add getFormatDate() to jask.date

--- src/function/InternalFunctionDate.java
+++ src/function/InternalFunctionDate.java
@@ -1,8 +1,11 @@
 package function;
 
+import java.text.SimpleDateFormat;
 import java.time.Instant;
+import java.util.Date;
 import java.util.Map;
 import variable.Variable;
+import helper.Error;
 
 /**
  * Implements the internal module date.os
@@ -21,6 +24,12 @@ public class InternalFunctionDate extends InternalFunctionsBase {
 				return getUnixTime(heap, params);
 			}
 		});
+		this.functions.put("getFormatDate", new InternalFunction() {
+			@Override
+			public Variable execute(Map<String, Variable> heap, String functionName, String param, String[] params) {
+				return getFormatDate(heap, params);
+			}
+		});
 	}
 	
 	/**
@@ -32,5 +41,42 @@ public class InternalFunctionDate extends InternalFunctionsBase {
 	 */
 	private Variable getUnixTime(Map<String, Variable> heap, String[] params) {
 		return new Variable((float)Instant.now().getEpochSecond());
+	}
+	
+	/**
+	 * Internal implementation of getFormatDate
+	 * 
+	 * @param heap function heap
+	 * @param params function parameters
+	 * @return new Variable containing the formatted date
+	 */
+	private Variable getFormatDate(Map<String, Variable> heap, String[] params) {
+		Variable formatString = heap.get(params[0]);
+		if (formatString == null) {
+			formatString = new Variable(heap.get(params[0]));
+		}
+		
+		Variable date = new Variable();
+		SimpleDateFormat dateFormat = new SimpleDateFormat();
+		
+		try {
+			dateFormat.applyPattern(formatString.toString());
+		}
+		catch (IllegalArgumentException e) {
+			Error.printErrorDateFormatIllegal(formatString.toString());
+		}
+		
+		if (params.length == 2) {
+			Variable unixSeconds = heap.get(params[1]);
+			if (unixSeconds == null) {
+				unixSeconds = new Variable(params[1]);
+			}
+			date.setStringValue(dateFormat.format(new Date((long)unixSeconds.getDoubleValue() * 1000L)));
+		}
+		else {
+			date.setStringValue(dateFormat.format(new Date()));
+		}
+		
+		return date;
 	}
 }
