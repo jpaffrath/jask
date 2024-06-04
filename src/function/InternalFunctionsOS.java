fbb4d134d99ff30c34907b4c5260d0424564aae8
Julius Paffrath <juliuspaffrath@web.de>
2024-06-04 10:57:27 +0200

    Catch parameter errors in internal OS functions

--- src/function/InternalFunctionsOS.java
+++ src/function/InternalFunctionsOS.java
@@ -1,6 +1,8 @@
 package function;
 
 import java.util.Map;
+
+import helper.Error;
 import variable.Variable;
 
 /**
@@ -17,6 +19,10 @@ public class InternalFunctionsOS extends InternalFunctionsBase {
 		this.functions.put("getOSName", new InternalFunction() {
 			@Override
 			public Variable execute(Map<String, Variable> heap, String functionName, String param, String[] params) {
+				if (heap.size() != 0) {
+					Error.printErrorFunctionIsNotExpectingParameters(functionName);
+				}
+				
 				String osName = System.getProperty("os.name");
 				return new Variable('"' + osName + '"');
 			}
@@ -24,6 +30,10 @@ public class InternalFunctionsOS extends InternalFunctionsBase {
 		this.functions.put("getOSArch", new InternalFunction() {
 			@Override
 			public Variable execute(Map<String, Variable> heap, String functionName, String param, String[] params) {
+				if (heap.size() != 0) {
+					Error.printErrorFunctionIsNotExpectingParameters(functionName);
+				}
+				
 				String osArch = System.getProperty("os.arch");
 				return new Variable('"' + osArch + '"');
 			}
@@ -31,6 +41,10 @@ public class InternalFunctionsOS extends InternalFunctionsBase {
 		this.functions.put("getOSVersion", new InternalFunction() {
 			@Override
 			public Variable execute(Map<String, Variable> heap, String functionName, String param, String[] params) {
+				if (heap.size() != 0) {
+					Error.printErrorFunctionIsNotExpectingParameters(functionName);
+				}
+				
 				String osVersion = System.getProperty("os.version");
 				return new Variable('"' + osVersion + '"');
 			}
