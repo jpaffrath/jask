625ced60d883a502e4677eb22fb30017d05f6ad1
Julius Paffrath <github@julius-paffrath.de>
2025-02-19 21:02:17 +0100

    Rewrite tokenizer from scratch

--- src/jask/Tokenizer.java
+++ src/jask/Tokenizer.java
@@ -1,18 +1,9 @@
 package jask;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.StringReader;
-import java.nio.charset.Charset;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
-
 import helper.Error;
 import helper.Helpers;
 
@@ -20,158 +11,154 @@ import helper.Helpers;
  * Tokenizer class
  *
  * @author Julius Paffrath
- *
  */
 public class Tokenizer {
 
-	/**
-	 * Checks if a given character is a valid split character
-	 *
-	 * @param character character to check
-	 * @return true if the given character is a valid split character, otherwise false
-	 */
-	private boolean isSplitCharacter(char character) {
-		switch (character) {
-		case 0x20: // whitespace
-		case 0x9:  // tab
-			return true;
-		default:
-			return false;
-		}
-	}
+    /**
+     * Checks if a given character is a valid split character.
+     *
+     * @param character character to check
+     * @return true if the given character is a valid split character, otherwise false
+     */
+    private boolean isSplitCharacter(char character) {
+        return character == ' ' || character == '\t';
+    }
 
-	/**
-	 * Parses input from given Reader to list of string tokens
-	 *
-	 * @param reader input
-	 * @return list of string tokens
-	 */
-	private List<String> parseInput(Reader reader) {
-		List<String> tokens = new ArrayList<>();
-		StringBuilder tokenBuffer = new StringBuilder();
+    /**
+     * Parses input from given Reader to list of string tokens.
+     *
+     * @param reader input
+     * @return list of string tokens
+     */
+    private List<String> parseInput(Reader reader) {
+        List<String> tokens = new ArrayList<>();
+        StringBuilder tokenBuffer = new StringBuilder();
+        
+        try (BufferedReader buffer = new BufferedReader(reader)) {
+            boolean insideString   = false;
+            boolean insideComment  = false;
+            boolean insideFunction = false;
+            boolean lastWasSplit   = false;
+            int val;
 
-		Reader buffer = new BufferedReader(reader);
+            while ((val = buffer.read()) != -1) {
+            	char curChar = (char)val;
 
-		boolean insideString = false;
-		boolean insideComment = false;
-		boolean insideFunction = false;
-		boolean lastWasSplit =  false;
+                if (curChar == '\r') {
+                	continue;
+                }
+                
+                // continue if multiple separator characters are detected
+                if (isSplitCharacter(curChar) && lastWasSplit && !insideString) {
+                	continue;
+                }
+                
+                // toggle comment parsing
+                if (curChar == ';' && !insideComment && !insideString) {
+                	insideComment = true;
+                }
+                
+                // toggle function parsing
+                if (curChar == '(' && !insideComment && !insideString) {
+                	insideFunction = true;
+                }
+                
+                if (curChar == ')' && !insideComment && !insideString) {
+                	insideFunction = false;
+                }
+                
+                if (isSplitCharacter(curChar) && insideFunction && !insideString) {
+                	continue;
+                }
+                
+                // turn newline to space and stop comment parsing
+                if (curChar == '\n') {
+                	curChar = ' ';
+                	insideComment = false;
+                }
+                
+                if (insideComment) {
+                	continue;
+                }
+                
+                // toggle string parsing
+                if (curChar == '"') {
+                	insideString = !insideString;
+                }
+                
+                // split sequence to token
+                if (isSplitCharacter(curChar) && !insideString) {
+                    if (tokenBuffer.length() > 0) {
+                        tokens.add(tokenBuffer.toString());
+                        tokenBuffer.setLength(0);
+                    }
+                    lastWasSplit = true;
+                    continue;
+                }
 
-		int val;
-		char curChar;
+                tokenBuffer.append(curChar);
+                lastWasSplit = false;
+            }
 
-		try {
-			while ((val = buffer.read()) != -1) {
-				curChar = (char)val;
+            if (tokenBuffer.length() > 0) {
+                tokens.add(tokenBuffer.toString());
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
 
-				// skip special characters
-				switch (curChar) {
-				case 0x0D:
-					continue;
-				}
+        return tokens;
+    }
 
-				// continue if multiple separator characters are detected
-				if (this.isSplitCharacter(curChar) && lastWasSplit && !insideString) {
-					continue;
-				}
+    /**
+     * Splits the given input file into tokens.
+     *
+     * @param file a jask file as input
+     * @return parsed list of tokens
+     */
+    public List<String> parse(File file) {
+        if (Helpers.checkFile(file) == false) {
+            return null;
+        }
+        
+        try (InputStream inputStream = new FileInputStream(file);
+             InputStreamReader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8)) {
+            return parseInput(reader);
+        }
+        catch (FileNotFoundException e) {
+        	Error.printErrorFileNotFound(file.getName());
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
 
-				// toggle comment parsing
-				if (curChar == ';' && !insideComment && !insideString) {
-					insideComment = true;
-				}
+        return null;
+    }
 
-				// toggle function parsing
-				if (curChar == '(' && !insideComment && !insideString) {
-					insideFunction = true;
-				}
-				else if (curChar == ')' && !insideComment && !insideString) {
-					insideFunction = false;
-				}
+    /**
+     * Splits the given input line into tokens.
+     *
+     * @param input a line of jask code
+     * @return parsed list of tokens
+     */
+    public List<String> parse(String input) {
+        return parseInput(new StringReader(input));
+    }
 
-				if (this.isSplitCharacter(curChar) && insideFunction && !insideString) {
-					continue;
-				}
+    /**
+     * Splits the given input list into tokens.
+     *
+     * @param inputList list of jask code
+     * @return parsed list of tokens
+     */
+    public List<String> parse(List<String> inputList) {
+        List<String> tokens = new ArrayList<>();
 
-				// turn newline to space and stop comment parsing
-				if (curChar == '\n') {
-					curChar = ' ';
-					insideComment = false;
-				}
+        for (String line : inputList) {
+            tokens.addAll(parse(line));
+        }
 
-				if (insideComment) {
-					continue;
-				}
-
-				// toggle string parsing
-				if (curChar == '"') {
-					insideString = !insideString;
-				}
-
-				// split sequence to token
-				if (this.isSplitCharacter(curChar) && !insideString) {
-					tokens.add(tokenBuffer.toString());
-					tokenBuffer = new StringBuilder();
-					lastWasSplit = true;
-					continue;
-				}
-
-				tokenBuffer.append(curChar);
-				lastWasSplit = false;
-			}
-		} catch (IOException e) { e.printStackTrace(); }
-
-		tokens.add(tokenBuffer.toString());
-
-		try { buffer.close(); } catch (IOException e) { e.printStackTrace(); }
-		return tokens;
-	}
-
-	/**
-	 * Splits the given input file into tokens
-	 *
-	 * @param file a jask file as input
-	 * @return parsed list of tokens
-	 */
-	List<String> parse(File file) {
-		if (!Helpers.checkFile(file)) {
-			return null;
-		}
-
-		InputStream inputStream = null;
-
-		try {
-			inputStream = new FileInputStream(file);
-		} catch (FileNotFoundException e) {
-			Error.printErrorFileNotFound(file.getName());
-			return null;
-		}
-
-		return this.parseInput(new InputStreamReader(inputStream, Charset.forName("UTF-8")));
-	}
-
-	/**
-	 * Splits the given input line into tokens
-	 *
-	 * @param input a line of jask code
-	 * @return parsed list of tokens
-	 */
-	List<String> parse(String input) {
-		return this.parseInput(new StringReader(input));
-	}
-
-	/**
-	 * Splits the given input list into tokens
-	 *
-	 * @param inputList list of jask code
-	 * @return parsed list of tokens
-	 */
-	List<String> parse(List<String> inputList) {
-		List<String> tokens = new ArrayList<>();
-
-		for (String line : inputList) {
-			tokens.addAll(this.parse(line));
-		}
-
-		return tokens;
-	}
+        return tokens;
+    }
 }
